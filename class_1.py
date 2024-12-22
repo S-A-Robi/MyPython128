33bcbe1a99395eb49a41b75e134251d6509afa94
S-A-Robi <robi.baec@gmail.com>
2024-12-22 20:35:15 +0600

    first commit

---
+++ class_1.py
@@ -0,0 +1,48 @@
+# print("Hello world")
+# print("Hello dhaka")
+# print("I live in Dhaka")
+# print("Hello Banani")
+# print("Ok save")
+
+#x = 5
+#y = "john"
+
+#print("x")
+#print("y")
+#print(y)
+
+
+#print('Enter your name:')
+
+#x = int(1)   # x will be 1
+#y = int(2.8) # y will be 2
+#z = int("3") # z will be 3
+
+#print(x,y,z)
+
+#x = float(1)     # x will be 1.0
+#y = float(2.8)   # y will be 2.8
+#z = float("3")   # z will be 3.0
+#w = float("4.2") # w will be 4.2
+
+#print(x,y,z,w)
+
+fruits =["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+    #for a in range(0, 20):
+       # print(a)
+
+
+#def my_function():
+   # print("hello function")
+    #print("hello function")
+    #print("hello function")
+
+#my_function()
+
+#a = 5
+#b = 3
+#result = a * b
+#print(result)
